63345e0aac6451790988a5aaa509050fe4df1822
ericlovesmath <ericlovesmath@gmail.com>
2024-01-03 23:33:46 -0800

    Comprehensive tests for Tokenizer, added next and peek methods

--- rust/src/tokenizer.rs
+++ rust/src/tokenizer.rs
@@ -1,6 +1,7 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 
+/** Reads tokens from given string */
 struct Tokenizer {
     tokens: Vec<String>,
     pos: usize,
@@ -13,6 +14,7 @@ lazy_static! {
 }
 
 impl Tokenizer {
+    /** Turns `s` into tokens to be parsed */
     pub fn new(s: String) -> Self {
         let tokens = TOKENIZER_RE
             .captures_iter(&s)
@@ -22,6 +24,26 @@ impl Tokenizer {
             })
             .collect();
         Self { tokens, pos: 0 }
+    }
+
+    /** Returns current `token`, advances `Tokenizer`.
+    Returns `None` if Tokenizer is completely consumed. */
+    pub fn next(&mut self) -> Option<&str> {
+        if self.pos >= self.tokens.len() {
+            return None;
+        };
+        let token = &self.tokens[self.pos];
+        self.pos += 1;
+        Some(token)
+    }
+
+    /** Returns current token, doesn't advance Tokenizer
+    Returns `None` if Tokenizer is completely consumed. */
+    pub fn peek(&mut self) -> Option<&str> {
+        if self.pos >= self.tokens.len() {
+            return None;
+        };
+        Some(&self.tokens[self.pos])
     }
 }
 
@@ -49,7 +71,35 @@ mod tests {
             "(def! nil?   (fn* [x] (= x nil  )))",
             "( def! nil? ( fn* [ x ] ( = x nil ) ) )",
         );
+        test("(+ 1 2) ;;Comment", "( + 1 2 ) ;;Comment");
+        test(
+            "(defmacro! let*B (fn* [binds form]
+                (let* [f (fn* [key val acc]
+                    `((fn* [~key] ~acc) ~val))]
+                        (_foldr_pairs f form binds))))",
+            concat!(
+                r"( defmacro! let*B ( fn* [ binds form ] ( let* [ f ( fn* ",
+                r"[ key val acc ] ` ( ( fn* [ ~ key ] ~ acc ) ~ val ) ) ] ",
+                r"( _foldr_pairs f form binds ) ) ) )",
+            ),
+        );
 
         assert!(Tokenizer::new(String::new()).tokens.is_empty());
+    }
+
+    #[test]
+    fn test_next_peek() {
+        let mut tk = Tokenizer::new(String::from(" a b   c d     "));
+        assert_eq!(tk.peek(), Some("a"));
+        assert_eq!(tk.next(), Some("a"));
+        assert_eq!(tk.peek(), Some("b"));
+        assert_eq!(tk.next(), Some("b"));
+        assert_eq!(tk.next(), Some("c"));
+        assert_eq!(tk.peek(), Some("d"));
+        assert_eq!(tk.next(), Some("d"));
+        assert_eq!(tk.peek(), None);
+        assert_eq!(tk.next(), None);
+        assert_eq!(tk.next(), None);
+        assert_eq!(tk.peek(), None);
     }
 }
