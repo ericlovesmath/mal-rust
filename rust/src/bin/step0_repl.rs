d5f18efd25282defbf0f17e66e2f4f0075010174
ericlovesmath <ericlovesmath@gmail.com>
2024-01-03 17:48:56 -0800

    MAL Step 0 (REPL) Complete

--- rust/src/bin/step0_repl.rs
+++ rust/src/bin/step0_repl.rs
@@ -1,5 +1,35 @@
 extern crate mal_rust;
 
+use std::io::{stdin, stdout, Write};
+
+fn read(text: &str) -> &str {
+    text
+}
+fn eval(text: &str) -> &str {
+    text
+}
+fn print(text: &str) -> &str {
+    text
+}
+
+fn rep(input: &str) -> &str {
+    print(eval(read(input)))
+}
+
 fn main() {
-    println!("Hello world!");
+    let stdin = stdin();
+    let mut stdout = stdout();
+    let mut buf = String::new();
+    loop {
+        print!("user> ");
+        stdout.flush().expect("Failed to flush()");
+
+        buf.clear();
+        match stdin.read_line(&mut buf) {
+            Ok(0) => break,
+            Ok(_) => (),
+            Err(err) => panic!("Failed to read_line(), {}", err),
+        }
+        print!("{}", rep(&buf))
+    }
 }
