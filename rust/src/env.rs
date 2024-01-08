0f08faa3b2617c19093fe42fba07dfc060275088
ericlovesmath <ericlovesmath@gmail.com>
2024-01-08 11:57:39 -0800

    Step 3 Final cleanup is a more ✨Idiomatic✨ way

--- rust/src/env.rs
+++ rust/src/env.rs
@@ -10,35 +10,23 @@ pub struct EnvStruct {
     data: HashMap<String, Sexp>,
 }
 
-pub fn env_new(outer: Option<Env>) -> Env {
+fn env_new(outer: Option<Env>) -> Env {
     Rc::new(RefCell::new(EnvStruct {
         outer,
         data: HashMap::new(),
     }))
 }
 
-pub fn env_default() -> Env {
-    let env = env_new(None);
-    env.borrow_mut().set("+".to_string(), &Sexp::Func(add));
-    env.borrow_mut().set("-".to_string(), &Sexp::Func(subtract));
-    env.borrow_mut().set("*".to_string(), &Sexp::Func(multiply));
-    env.borrow_mut().set("/".to_string(), &Sexp::Func(divide));
-    env
-}
-
 impl EnvStruct {
-    pub fn set(&mut self, symbol: String, sexp: &Sexp) {
-        self.data.insert(symbol, sexp.clone());
+    fn set(&mut self, symbol: &str, sexp: Sexp) {
+        self.data.insert(symbol.to_string(), sexp);
     }
 
     fn get(&self, symbol: &str) -> Option<Sexp> {
-        match self.data.get(symbol) {
-            None => match &self.outer {
-                None => None,
-                Some(env) => env.borrow().get(symbol),
-            },
-            Some(sexp) => Some(sexp.clone()),
-        }
+        self.data
+            .get(symbol)
+            .cloned()
+            .or(self.outer.as_ref().and_then(|env| env.borrow().get(symbol)))
     }
 }
 
@@ -57,23 +45,29 @@ pub fn evaluate(ast: Sexp, env: Env) -> Result<Sexp, String> {
                 ));
             };
             let eval = evaluate(val.clone(), env.clone())?;
-            env.borrow_mut().set(key.clone(), &eval);
+            env.borrow_mut().set(key, eval.clone());
             Ok(eval)
         }
-        Sexp::List(list) if first_equal(&list, "let*") => {
-            let [_, Sexp::List(list), val] = list.as_slice() else {
+        Sexp::List(list) | Sexp::Vec(list) if first_equal(&list, "let*") => {
+            let ([Sexp::List(list), val] | [Sexp::Vec(list), val]) = &list[1..] else {
                 return Err(format!(
                     "let* expected [Keys, Val], got {}",
                     tokens_to_string(&list)
                 ));
             };
+            if list.len() % 2 == 1 {
+                return Err(format!(
+                    "let* recieved an odd number of atoms on LHS: [{}]",
+                    tokens_to_string(list)
+                ));
+            }
             let env = env_new(Some(env.clone()));
             for chunk in list.chunks_exact(2) {
                 let [Sexp::Symbol(sym), expr] = chunk else {
                     return Err("let* did not recieve Sexp::Symbol".to_string());
                 };
                 let eval = evaluate(expr.clone(), env.clone())?;
-                env.borrow_mut().set(sym.clone(), &eval);
+                env.borrow_mut().set(sym, eval);
             }
             evaluate(val.clone(), env.clone())
         }
@@ -90,17 +84,21 @@ pub fn evaluate(ast: Sexp, env: Env) -> Result<Sexp, String> {
     }
 }
 
+fn eval_sexps(sexps: Vec<Sexp>, env: Env) -> Result<Vec<Sexp>, String> {
+    sexps
+        .into_iter()
+        .map(|s| evaluate(s, env.clone()))
+        .collect::<Result<Vec<Sexp>, _>>()
+}
+
 fn apply(ast: Sexp, env: Env) -> Result<Sexp, String> {
     match ast {
         Sexp::Symbol(sym) => env
             .borrow()
             .get(sym.as_ref())
             .ok_or(format!("Unknown symbol '{}' found", sym)),
-        Sexp::List(list) => list
-            .into_iter()
-            .map(|s| evaluate(s, env.clone()))
-            .collect::<Result<Vec<Sexp>, _>>()
-            .map(Sexp::List),
+        Sexp::List(list) => eval_sexps(list, env).map(Sexp::List),
+        Sexp::Vec(list) => eval_sexps(list, env).map(Sexp::Vec),
         ast => Ok(ast),
     }
 }
@@ -123,6 +121,18 @@ arithmetic_op!(add, +);
 arithmetic_op!(subtract, -);
 arithmetic_op!(multiply, *);
 arithmetic_op!(divide, /);
+
+pub fn env_default() -> Env {
+    let env = env_new(None);
+    {
+        let mut env = env.borrow_mut();
+        env.set("+", Sexp::Func(add));
+        env.set("-", Sexp::Func(subtract));
+        env.set("*", Sexp::Func(multiply));
+        env.set("/", Sexp::Func(divide));
+    }
+    env
+}
 
 #[cfg(test)]
 mod tests {
